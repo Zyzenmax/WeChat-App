69fc017fafedec54fd794ff646bb0836ef27e512
Zyzenbeep <bagushasanali78@gmail.com>
2025-01-22 08:54:46 +0700

    Simple Animations

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:we_chat/screens/home_screen.dart';
 late Size mq;
 void main() {
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:we_chat/main.dart';
+import 'package:we_chat/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,9 +11,21 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _isAnimate = true; // Memicu rebuild untuk animasi
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    mq = MediaQuery.of(context).size;
+    final mq = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
@@ -21,26 +34,32 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: Stack(
         children: [
+          AnimatedPositioned(
+            top: mq.height * .10,
+            right: _isAnimate ? mq.width * .25 : mq.width * .5,
+            width: mq.width * .5,
+            duration: const Duration(seconds: 1),
+            child: Image.asset('images/icon.png'),
+          ),
           Positioned(
-              top: mq.height * .10,
-              left: mq.width * .25,
-              width: mq.width * .5,
-              child: Image.asset('images/icon.png')),
-          Positioned(
-              bottom: mq.height * .15,
-              left: mq.width * .05,
-              width: mq.width * .9,
-              height: mq.height * .07,
-              child: ElevatedButton.icon(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.blue[300]),
-                onPressed: () {},
-                icon: Image.asset('images/google.png', height: mq.height * .03),
-                label: const Text(
-                  'Sign In with Google',
-                  style: TextStyle(color: Colors.black, fontSize: 19),
-                ),
-              ))
+            bottom: mq.height * .15,
+            left: mq.width * .05,
+            width: mq.width * .9,
+            height: mq.height * .07,
+            child: ElevatedButton.icon(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.blue[300]),
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => HomeScreen()));
+              },
+              icon: Image.asset('images/google.png', height: mq.height * .03),
+              label: const Text(
+                'Login with Google',
+                style: TextStyle(color: Colors.black, fontSize: 19),
+              ),
+            ),
+          ),
         ],
       ),
     );
